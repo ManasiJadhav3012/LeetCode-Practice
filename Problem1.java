85551b72b8b53d8beecad1afee495c37b9cb5971
Manasi Jadhav <manasijadhav@Manasis-MacBook-Pro-2.local>
2024-06-26 22:18:44 +0530

    Done with 2 interview questions

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,48 @@
+// Find Missing Number in a sorted array
 
+class MissingNumber {
+
+    int findMissingNumber(int[] arr) {
+        
+        if ( arr == null || arr.length < 2) {
+            return -1;
+        }
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(arr[mid] != mid + 1) {
+
+                if( arr[mid] == mid + 2 ) {
+                    return mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main (String args[]) {
+
+        MissingNumber mn = new MissingNumber();
+
+        int[] arr1 = {1, 2, 4, 5, 6};
+        int[] arr2 = {1, 2, 3, 4, 5};
+        int[] arr3 = {1, 2, 3, 4, 5, 7};
+
+        System.out.println(mn.findMissingNumber(arr1));
+        System.out.println(mn.findMissingNumber(arr2));
+        System.out.println(mn.findMissingNumber(arr3));
+
+
+    }
+
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,111 @@
+// Implementation of MinHeap using Array
 
+class MinHeap {
+    private int[] heap;
+    private int size; 
+    private int capacity;
+
+    public MinHeap (int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.heap = new int[this.capacity + 1];
+        this.heap[0] = Integer.MIN_VALUE;
+    }
+
+    private int parent (int pos) {
+        return pos / 2;
+    }
+
+    private int leftChild (int pos) {
+        return pos * 2;
+    }
+
+    private int rightChild (int pos) {
+        return (pos * 2) + 1;
+    }
+
+    private boolean isLeaf (int pos) {
+        return pos > (size / 2) && pos <= size;
+    }
+
+    private int size() {
+        return size;
+    }
+
+    private void print() {
+        for (int i = 1; i <= size; i++) {
+            System.out.print(" " + heap[i]);
+        }
+        System.out.println();
+    }
+
+    private int peek() {
+        return heap[1];
+    }
+
+    private void swap (int pos1, int pos2) {
+        int temp;
+        temp = heap[pos1];
+        heap[pos1] = heap[pos2];
+        heap[pos2] = temp;
+    }
+
+    private void minHeapify (int pos) {
+        if (!isLeaf(pos)) {
+            if (heap[pos] > heap[leftChild(pos)] || heap[pos] > heap[rightChild(pos)]) {
+                if (heap[leftChild(pos)] < heap[rightChild(pos)]) {
+                    swap (pos, leftChild(pos));
+                    minHeapify(leftChild(pos));
+                } else {
+                    swap(pos, rightChild(pos));
+                    minHeapify(rightChild(pos));
+                }
+            }
+        }
+    }
+
+    public void add (int element) {
+        if (size >= capacity) {
+            return;
+        }
+
+        size = size + 1;
+        heap[size] = element;
+
+        int current = size;
+
+        while (heap[current] < heap[parent(current)]) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+
+    public int remove() {
+        int poppedElement = heap[1];
+        heap[1] = heap[size];
+        size = size - 1;
+        minHeapify(1);
+        return poppedElement;
+    }
+
+    public static void main (String[] args) {
+        MinHeap minHeap = new MinHeap(15);
+
+        minHeap.add(5);
+        minHeap.add(3);
+        minHeap.add(7);
+        minHeap.add(6);
+        minHeap.add(4);
+
+        minHeap.print();
+        
+
+        System.out.println("Size: " + minHeap.size() + " Peek: " + minHeap.peek());
+
+        minHeap.remove();
+        minHeap.print();
+
+        minHeap.remove();
+        minHeap.print();
+    }
+}
